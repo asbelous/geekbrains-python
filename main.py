18db3e3eb3bcad7e7b678e4f851cf9de000c1821
Алексей Белоусов <arcaim@list.ru>
2020-07-21 23:03:00 +0700

    Первый урок

---
+++ main.py
@@ -0,0 +1,29 @@
+# 1 задание
+
+#number = int(input('Введите число: '))
+#print('Ваш результат: ', number + 2)
+
+# 2 задание
+
+# while True:
+#     number = int(input('Введите число больше 0 и меньше 10: '))
+#     if(number <= 0 or number >= 10):
+#         print('Число не подходит')
+#         continue
+#     else:
+#         print('Ваш результат: ', number ** 2)
+#         break
+
+# 3 задание
+
+# name = input('Введите имя: ')
+# surname = input('Введите фамилию: ')
+# age = int(input('Введите возраст: '))
+# weight = int(input('Введите вес: '))
+#
+# if(age <= 30 and weight >= 50 or weight <= 120):
+#     print(name, " ", surname, ", возраст - ", age, ", вес - ", weight, " кг: в хорошем состоянии!", sep='')
+# if(age > 30 and weight < 50 or weight > 120):
+#     print(name, " ", surname, ", возраст - ", age, "вес - ", weight, " кг: займись собой!", sep='')
+# if(age > 40 and weight < 50 or weight > 120):
+#     print(name, " ", surname, ", возраст - ", age, "вес - ", weight, " кг: обратись к врачу!", sep='')
