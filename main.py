64a6a21660b60e58cd40c9894a1956be29ec1fb1
Алексей Белоусов <arcaim@list.ru>
2020-07-28 00:44:28 +0700

    Решил 4 задание

--- main.py
+++ main.py
@@ -27,3 +27,13 @@
 #     print(name, " ", surname, ", возраст - ", age, "вес - ", weight, " кг: займись собой!", sep='')
 # if(age > 40 and weight < 50 or weight > 120):
 #     print(name, " ", surname, ", возраст - ", age, "вес - ", weight, " кг: обратись к врачу!", sep='')
+
+# 4 Задание
+
+# my_list_1 = [2, 5, 8, 2, 2, 12, 12, 4]
+# my_list_2 = [2, 7, 12, 3]
+#
+# for num in my_list_2:
+#     while num in my_list_1:
+#         my_list_1.remove(num)
+# print(my_list_1)
