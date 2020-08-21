c6d6bf657cd723b5d11e6cdd153183ab5b695f2a
Алексей Белоусов <asbelous@bk.ru>
2020-08-21 19:32:14 +0700

    Решил 16 задание

--- main.py
+++ main.py
@@ -237,3 +237,11 @@
 # result = [fruit for fruit in fruits1 if fruit in fruits2]
 #
 # print(result)
+
+# 16 Задание
+
+# numbers = [1, 5, -9, 10, 27, 8, 17, 45, 100, 11, -7, 12, -27]
+#
+# result = [number for number in numbers if number > 0 and number % 3 == 0 and number % 4 != 0]
+#
+# print(result)
