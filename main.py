af7a6ab35d520659b899c310ea6d68034a2b1ef1
Алексей Белоусов <asbelous@bk.ru>
2020-08-27 14:20:10 +0700

    Решил 18 задание

--- main.py
+++ main.py
@@ -267,3 +267,21 @@
 # result = new_sqrt_list(old_list)
 # print(result)
 # print(old_list)
+
+# 18 Задание
+
+# def unlucky_number(number):
+#     if number == 13:
+#         raise ValueError('Несчастливое число')
+#     else:
+#         return number ** 2
+#
+#
+# number = int(input('Введите число: '))
+#
+# try:
+#     result = unlucky_number(number)
+# except ValueError:
+#     print('У нас несчастливое число')
+# else:
+#     print(result)
